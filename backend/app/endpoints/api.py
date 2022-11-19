94e7f12d3e518f749b52069725f31d71329dd22e
dimple5 <dimple@vernacular.ai>
2022-11-19 14:57:23 +0100

    added singleton to the api

--- backend/app/endpoints/api.py
+++ backend/app/endpoints/api.py
@@ -7,27 +7,34 @@ from app.integration.api_helper import make_request
 
 from app.integration.redis import RedisClient
 
+from backend.app.worker.manager import Manager
+
 router = APIRouter()
 
 
 @router.get("/crypto/sign")
 async def sign_message(message: str, webhook: str):
     """
-    The API endpoin forwards the request to hiring api. This is encapsulated in a task
-    that runs in the background. To intiate the task the message is added the queue and
-    the last keeps listening to the queue and makes request to the external api and once the result is retrieved the
-    response is posted back to the webhook.
-
-    :param message: client message
-    :param webhook: callqack url
-    :param redis_client: dependency
-    :return:
+    This api returns the signature of the message. The api is supported by Redis cache. If the api is unable to get response from api
+    the message is stored in the queue and the api returns 202 status code. The api is supported by RabbitMQ queue.
+    :param message: message to be signed
+    :param webhook: webhook to be called after signing the message
+    :return: signature of the message
     """
     redis_client = RedisClient.Instance().get_client()
     if settings.CACHE_ACTIVE:
         if redis_client.get(message) is not None:
             return redis_client.get(message)
 
+    # initiate the manager for single threaded execution
+    result = Manager.Instance().process_single_message(message)
+
+    if result is not None:
+        if settings.CACHE_ACTIVE:
+            redis_client.set(message, result)
+        return result
+
+    # if the api is unable to get response from api the message is stored in the queue
     publish_message(message, webhook)
 
     raise HTTPException(
@@ -39,7 +46,7 @@ async def sign_message(message: str, webhook: str):
 @router.get("/crypto/verify")
 def verify_signature(message: str, signature: str):
     """
-    Verify a message signature.
+    The api verifies the signature of the message.
     :param message:
     :param signature:
     :return:
